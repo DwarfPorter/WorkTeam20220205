583db42dec355ab7cfbbb80eee52941b8ee7b2f6
Vladimir Morozov <mrvlmor@gmail.com>
2022-02-05 15:50:50 +0300

    Update MainActivity.kt

--- app/src/main/java/ru/geekbrains/workteam20220205/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/workteam20220205/MainActivity.kt
@@ -2,10 +2,16 @@ package ru.geekbrains.workteam20220205
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        findViewById<Button>(R.id.button).setOnClickListener{
+            findViewById<TextView>(R.id.button).text = "done"
+        }
+        
     }
 }
