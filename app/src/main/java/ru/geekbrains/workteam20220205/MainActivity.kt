35beef39f962c3124b1d10bdfbebb8adb7298cf9
Vladimir Morozov <mrvlmor@gmail.com>
2022-02-05 15:54:11 +0300

    обработка кнопки (#3)

--- app/src/main/java/ru/geekbrains/workteam20220205/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/workteam20220205/MainActivity.kt
@@ -2,11 +2,19 @@ package ru.geekbrains.workteam20220205
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.ok)
+        button.setOnClickListener {
+            val text = findViewById<TextView>(R.id.text)
+            text.text = "OK"
+        }
     }
     
     private val tempField: String = "TEST"
